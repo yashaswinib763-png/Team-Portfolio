34308ee93a9ed6ff78fffd396fd05e2c7ea3351a
Bagalli yahaswini <yashaswinib763@gmail.com>
2025-10-05 22:28:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
